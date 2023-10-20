5042e61e090720500b69a844b4721ac2b57f9dd8
Kervin Espirito Santo <57775586+kervines@users.noreply.github.com>
2023-10-20 15:50:55 +0000

    finalizada implementaçao do `script.js`

--- script.js
+++ script.js
@@ -4,16 +4,15 @@ const number1 = document.querySelector(".number1");
 const number2 = document.querySelector(".number2");
 const input = document.querySelector(".input");
 
-const randomNumber = () => {
-  return Math.floor(Math.random() * 11);
+const randomNumber = (num) => {
+  return (num.innerText = Math.floor(Math.random() * 11));
 };
 
-const num1 = (number1.innerText = randomNumber());
-const num2 = (number2.innerText = randomNumber());
+let num1 = randomNumber(number1);
+let num2 = randomNumber(number2);
 
-const atualizaNumber = (num) => {
-  return (num.innerText = randomNumber());
-};
+console.log(num1);
+console.log(num2);
 
 let point = 0;
 const startGame = () => {
@@ -21,13 +20,18 @@ const startGame = () => {
     point++;
     score.innerText = point;
     input.value = "";
+    num1 = randomNumber(number1);
+    num2 = randomNumber(number2);
   } else {
     point--;
     score.innerText = point;
     input.value = "";
+    num1 = randomNumber(number1);
+    num2 = randomNumber(number2);
   }
-  atualizaNumber(num1);
-  atualizaNumber(num2);
+
+  console.log(num1);
+  console.log(num2);
 };
 
 submit.addEventListener("click", startGame);
